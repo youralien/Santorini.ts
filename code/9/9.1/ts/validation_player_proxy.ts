0f38a92fc674c2461932aad9ca0dcf095867a419
youralien <ryanlouie2021@u.northwestern.edu>
2018-12-01 14:27:37 -0600

    ValidationPlayerProxy and Rm playoption non losing

---
+++ code/9/9.1/ts/validation_player_proxy.ts
@@ -0,0 +1,64 @@
+import { PlayerInterface} from "./player_interface";
+import {RuleChecker} from "./rules";
+
+export class ValidationPlayerProxy implements PlayerInterface {
+
+    wrapped_player: PlayerInterface;
+    name: string;
+    color: string;
+    turn: number;
+    rulecheck: RuleChecker;
+
+    constructor(wrapped_player: PlayerInterface) {
+        this.wrapped_player = wrapped_player;
+
+        // track what command turn it is, starts on expecting register
+        this.turn = 0;
+    }
+
+    commandsOutOfSequence() {
+        return "Santorini is broken! Too many tourists in such a small place...";
+    }
+
+    async register() {// : string | Promise<string> {
+        if (this.turn !== 0) {
+            return this.commandsOutOfSequence();
+        }
+        this.turn++;
+        return await this.wrapped_player.register();
+    }
+
+    /**
+     * Checks that should happen
+     * 1. Board should be a valid board (the right size, no one has won)
+     * 2. If its asking me to place white, there shouldn't be white players already on there
+     * 3. Should have 0 or 2 players on there
+     *
+     * @param {string} color
+     * @param {any[][]} board
+     */
+    async placeWorkers(color: string, board: any[][]) {
+        if (this.turn != 1) {
+            return this.commandsOutOfSequence();
+        }
+        this.turn++;
+        return await this.wrapped_player.placeWorkers(color, board);
+    }
+    async play(board: any[][]) {
+        if (this.turn < 1) {
+            return this.commandsOutOfSequence();
+        }
+        this.turn++;
+        return await this.wrapped_player.play(board);
+    }
+    async gameOver(name: string) {
+        this.turn++;
+        return await this.wrapped_player.gameOver(name);
+    }
+
+    reset() {
+        this.turn = 1;
+    }
+}
+
+export const Player = ValidationPlayerProxy;
